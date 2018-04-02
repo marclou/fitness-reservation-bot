6100342039706707714a352a8344d5d20219d3b1
marclou <marc.louvion@gmail.com>
2018-04-02 16:49:37 +0900

    adding all methods for workout model

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -19,29 +19,61 @@ router.get('/', (req, res) => {
 	});
 });
 
+router.get('/workout/:id', (req, res) => {
+	const workoutID = req.params.id;
+
+	if (!ObjectId.isValid(workoutID)) {
+		return res.status(404).send({ error: 'Invalid workout ID' });
+	}
+
+	Workout.findById(workoutID).populate('location').then((workout) => {
+		if (workout) {
+			res.status(200).send({ workout: workout.toJSON() });
+		}
+		return res.status(404).send({ error: 'Error with workout' });
+	}).catch((error) => {
+		res.status(400).send({ error });
+	});
+});
+
 router.post('/workout', (req, res) => {
 	const workoutToAdd = _.pick(req.body, ['name', 'location', 'duration', 'cost', 'date', 'miscellaneous']);
-	const workoutDoc = new Workout({ workoutToAdd });
+	const workoutDoc = new Workout(workoutToAdd);
 
 	workoutDoc.save().then((workout) => {
-		res.status(200).send({ workout });
+		res.status(200).send({ workout: workout.toJSON() });
 	}).catch((error) => {
 		res.status(400).send({ error });
 	});
 });
 
 router.delete('/workout/:id', (req, res) => {
-	const workoutIdToDelete = req.params.id;
+	const workoutID = req.params.id;
 
-	if (!ObjectId.isValid(workoutIdToDelete)) {
-		return res.status(404).send({ error: 'Invalid user ID' });
+	if (!ObjectId.isValid(workoutID)) {
+		return res.status(404).send({ error: 'Invalid workout ID' });
 	}
 
-	Workout.findByIdAndRemove(workoutIdToDelete).then((workout) => {
+	Workout.findByIdAndRemove(workoutID).then((workout) => {
 		if (workout) {
-			return res.status(200).send({ workout });
+			return res.status(200).send({ workout: workout.toJSON() });
 		}
 		res.status(404).send({ error: 'Workout doesn\'t exist' });
+	}).catch((error) => {
+		res.status(400).send({ error });
+	});
+});
+
+router.patch('/workout/:id', (req, res) => {
+	const workoutID = req.params.id;
+	const workoutUpdated = _.pick(req.body, ['name', 'location', 'duration', 'cost', 'date', 'miscellaneous']);
+
+	if (!ObjectId.isValid(workoutID)) {
+		return res.status(404).send({ error: 'Invalid workout ID' });
+	}
+
+	Workout.findByIdAndUpdate(workoutID, workoutUpdated, { new: true, runValidators: true }).then((workout) => {
+		res.status(200).send({ workout: workout.toJSON() });
 	}).catch((error) => {
 		res.status(400).send({ error });
 	});
