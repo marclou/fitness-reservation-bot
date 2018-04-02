775e912c28e6bcd6c2642d77672f420d13808222
marclou <marc.louvion@gmail.com>
2018-04-02 14:10:28 +0900

    split request to routers

--- server/app.js
+++ server/app.js
@@ -1,15 +1,15 @@
 require('./config/config');
+require('./db/mongoose');
 
-const mongoose = require('./db/mongoose');
 const express = require('express');
-
-const {
-	Gym,
-	Workout,
-} = require('./models/index');
+const { mainRouter, dashboardRouter, webhookRouter } = require('./routes/index');
 
 const app = express();
 const { PORT, MONGODB_URI } = process.env;
+
+app.use('/', mainRouter);
+app.use('/dashboard', dashboardRouter);
+app.use('/webhook', webhookRouter)
 
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}. \nMongoDB URI is : ${MONGODB_URI}`);
