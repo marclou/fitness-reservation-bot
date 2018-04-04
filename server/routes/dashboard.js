2e9d50e4d23e05ef7b19a8033fd47358c576b1a6
marclou <marc.louvion@gmail.com>
2018-04-04 22:05:19 +0900

    adding post method for adding a gym to DB

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -80,12 +80,26 @@ router.patch('/workout/:id', (req, res) => {
 	});
 });
 
-router.get('/gyms', (req, res) => {
+router.get('/gym', (req, res) => {
 	Gym.find({}).then((gyms) => {
 		if (!gyms) {
 			return res.status(404).send({ error: 'No gyms added yet.' });
 		}
 		res.status(200).send({ gyms });
+	}).catch((error) => {
+		res.status(400).send({ error });
+	});
+});
+
+router.post('/gym', (req, res) => {
+	const gymToAdd = _.pick(req.body, ['name', 'address', 'maxAttendance', 'facilities']);
+	const gymDoc = new Gym(gymToAdd);
+
+	gymDoc.save().then((gym) => {
+		if (!gym) {
+			return res.status(404).send({ error: 'Can\'t save gym' });
+		}
+		res.status(200).send({ gym });
 	}).catch((error) => {
 		res.status(400).send({ error });
 	});
