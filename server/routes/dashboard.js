d6f286c5ead0e4098d45e660b3236771f7592002
marclou <marc.louvion@gmail.com>
2018-04-16 12:24:55 +0200

    adding login logout and signup routes

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -4,8 +4,16 @@ const createError = require('http-errors');
 const workoutController = require('../controllers/workoutController');
 const gymController = require('../controllers/gymController');
 const broadcastController = require('../controllers/broadcastController');
+const adminController = require('../controllers/adminController');
+const config = require('../../config');
 
 const router = express.Router();
+const { authenticate } = adminController;
+
+// Router Middleware
+router.use(express.static(config.publicDir));
+router.use(['/workout', '/login', '/signup', '/broadcast'], express.static(config.publicDir));
+
 /**
  * Middleware that verifies all :ID parameters given in routes and test it's type
  * Throw an error if not a valid mongoDB ObjectID type
@@ -21,23 +29,33 @@ router.get('/', (req, res) => {
     res.redirect('/dashboard/workout');
 });
 
-router.get('/workout', workoutController.list);
+router.get('/workout', authenticate, workoutController.list);
 
-router.get('/workout/create', workoutController.getCreateForm);
+router.get('/workout/create', authenticate, workoutController.getCreateForm);
 
-router.post('/workout/create', workoutController.createOne);
+router.post('/workout/create', authenticate, workoutController.createOne);
 
-router.get('/workout/:id', workoutController.details);
+router.get('/workout/:id', authenticate, workoutController.details);
 
-router.post('/workout/:id/update', workoutController.updateOne);
+router.post('/workout/:id/update', authenticate, workoutController.updateOne);
 
-router.post('/workout/:id/delete', workoutController.deleteOne);
+router.post('/workout/:id/delete', authenticate, workoutController.deleteOne);
 
-router.get('/gym', gymController.list);
+router.get('/gym', authenticate, gymController.list);
 
-router.post('/gym', gymController.createOne);
+router.post('/gym', authenticate, gymController.createOne);
 
-router.get('/broadcast', broadcastController.index);
+router.get('/broadcast', authenticate, broadcastController.index);
+
+router.get('/login', adminController.getLoginForm);
+
+router.post('/login', adminController.login);
+
+router.get('/signup', adminController.getSignUpForm);
+
+router.post('/signup', adminController.createOne);
+
+router.get('/logout', authenticate, adminController.logout);
 
 module.exports = {
     dashboardRouter: router,
