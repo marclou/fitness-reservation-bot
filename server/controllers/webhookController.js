c554a81a2c77f8015cb48b32409dab8e05357107
marclou <marc.louvion@gmail.com>
2018-04-19 17:28:35 +0900

    typo

--- server/controllers/webhookController.js
+++ server/controllers/webhookController.js
@@ -1,45 +1,46 @@
 const request = require('request');
 const config = require('./../../config');
 
-module.exports = {
-    // Handles messages events
-    handleMessage: (sender_psid, received_message) => {
-        let response;
-        if (received_message.text) {
-            response = {
-                'text': `You sent the message: "${received_message.text}". Now send me an image!`,
-            };
-            console.log(response);
+// Sends response messages via the Send API
+const callSendAPI = (sender_psid, response) => {
+    const request_body = {
+        'recipient': {
+            'id': sender_psid,
+        },
+        'message': response,
+    };
+
+    request({
+        'uri': 'https://graph.facebook.com/v2.6/me/messages',
+        'qs': { 'access_token': config.PAGE_ACCESS_TOKEN },
+        'method': 'POST',
+        'json': request_body,
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!');
+        } else {
+            console.error(`Unable to send message:${err}`);
         }
-        this.callSendAPI(sender_psid, response);
-    },
+    });
+};
 
-    // Handles messaging_postbacks events
-    handlePostback: (sender_psid, received_postback) => {
-
-    },
-
-    // Sends response messages via the Send API
-    callSendAPI: (sender_psid, response) => {
-        const request_body = {
-            'recipient': {
-                'id': sender_psid,
-            },
-            'message': response,
+// Handles messages events
+const handleMessage = (sender_psid, received_message) => {
+    let response;
+    if (received_message.text) {
+        response = {
+            'text': `You sent the message: "${received_message.text}". Now send me an image!`,
         };
-        console.log('sending..');
+    }
+    callSendAPI(sender_psid, response);
+};
 
-        request({
-            'uri': 'https://graph.facebook.com/v2.6/me/messages',
-            'qs': { 'access_token': config.PAGE_ACCESS_TOKEN },
-            'method': 'POST',
-            'json': request_body,
-        }, (err, res, body) => {
-            if (!err) {
-                console.log('message sent!');
-            } else {
-                console.error(`Unable to send message:${err}`);
-            }
-        });
-    },
+// Handles messaging_postbacks events
+const handlePostback = (sender_psid, received_postback) => {
+
+};
+
+module.exports = {
+    handleMessage,
+    handlePostback,
 };
