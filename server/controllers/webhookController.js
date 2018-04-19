6ee13a009813e2547f8260bf0b81ccc9018f35e7
marclou <marc.louvion@gmail.com>
2018-04-19 17:16:39 +0900

    handle basic message posting

--- server/controllers/webhookController.js
+++ server/controllers/webhookController.js
@@ -1,7 +1,16 @@
+const request = require('request');
+const config = require('./../../config');
+
 module.exports = {
     // Handles messages events
     handleMessage: (sender_psid, received_message) => {
-
+        let response;
+        if (received_message.text) {
+            response = {
+                'text': `You sent the message: "${received_message.text}". Now send me an image!`
+            };
+        }
+        this.callSendAPI(sender_psid, response);
     },
 
     // Handles messaging_postbacks events
@@ -11,6 +20,24 @@ module.exports = {
 
     // Sends response messages via the Send API
     callSendAPI: (sender_psid, response) => {
+        const request_body = {
+            'recipient': {
+                'id': sender_psid,
+            },
+            'message': response,
+        };
 
+        request({
+            'uri': 'https://graph.facebook.com/v2.6/me/messages',
+            'qs': { 'access_token': config.PAGE_ACCESS_TOKEN },
+            'method': 'POST',
+            'json': request_body,
+        }, (err, res, body) => {
+            if (!err) {
+                console.log('message sent!');
+            } else {
+                console.error(`Unable to send message:${err}`);
+            }
+        });
     },
 };
--- server/routes/webhook.js
+++ server/routes/webhook.js
@@ -1,25 +1,19 @@
 const express = require('express');
 const config = require('./../../config');
+const webhookController = require('../controllers/webhookController');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    // Your verify token. Should be a random string.
     const { VERIFY_TOKEN } = config;
-    // Parse the query params
     const mode = req.query['hub.mode'];
     const token = req.query['hub.verify_token'];
     const challenge = req.query['hub.challenge'];
 
-    // Checks if a token and mode is in the query string of the request
     if (mode && token) {
-        // Checks the mode and token sent is correct
         if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-              // Responds with the challenge token from the request
-              console.log('WEBHOOK_VERIFIED');
               res.status(200).send(challenge);
         } else {
-            // Responds with '403 Forbidden' if verify tokens do not match
             res.sendStatus(403);
         }
     }
@@ -27,17 +21,17 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
     const { body } = req;
-    // Checks this is an event from a page subscription
-    if (body.object === 'page') {
-        // Iterates over each entry - there may be multiple if batched
-        body.entry.forEach((entry) => {
-            // Gets the message. entry.messaging is an array, but
-            // will only ever contain one message, so we get index 0
-            const webhook_event = entry.messaging[0];
-            console.log(webhook_event);
 
+    if (body.object === 'page') {
+        body.entry.forEach((entry) => {
+            const webhook_event = entry.messaging[0];
             const sender_psid = webhook_event.sender.id;
-            console.log('Sender PSID: ' + sender_psid);
+
+            if (webhook_event.message) {
+                webhookController.handleMessage(sender_psid, webhook_event.message);
+            } else if (webhook_event.postback) {
+                webhookController.handlePostback(sender_psid, webhook_event.postback);
+            }
         });
 
         res.status(200).send('EVENT_RECEIVED');
