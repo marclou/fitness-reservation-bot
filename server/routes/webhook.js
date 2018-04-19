150ed041ed3347ebd1cddf57eec7fc3bf1ca56ca
marclou <marc.louvion@gmail.com>
2018-04-19 13:19:26 +0900

    setup weebhook for messenger platform

--- server/routes/webhook.js
+++ server/routes/webhook.js
@@ -1,7 +1,46 @@
-const router = require('express').Router();
+const express = require('express');
+const config = require('./../../config');
+
+const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('<h1> Webhook URI </h1>');
+    // Your verify token. Should be a random string.
+    const VERIFY_TOKEN = config.secret;
+    // Parse the query params
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
+
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+        // Checks the mode and token sent is correct
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+              // Responds with the challenge token from the request
+              console.log('WEBHOOK_VERIFIED');
+              res.status(200).send(challenge);
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
+    }
+});
+
+router.post('/', (req, res) => {
+    const { body } = req;
+    // Checks this is an event from a page subscription
+    if (body.object === 'page') {
+        // Iterates over each entry - there may be multiple if batched
+        body.entry.forEach((entry) => {
+            // Gets the message. entry.messaging is an array, but
+            // will only ever contain one message, so we get index 0
+            const webhookEvent = entry.messaging[0];
+            console.log(webhookEvent);
+        });
+
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
+        res.sendStatus(404);
+    }
 });
 
 module.exports = {
