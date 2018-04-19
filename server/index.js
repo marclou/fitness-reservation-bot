cbc13f2c31c32b90b03b80b03e3945d63aa63d4a
marclou <marc.louvion@gmail.com>
2018-04-19 12:36:46 +0900

    render welcome message before error if user is not logged in

--- server/index.js
+++ server/index.js
@@ -38,11 +38,11 @@ module.exports = function () {
 		server.use((err, req, res, next) => {
 		  res.locals.message = err.message;
 		  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-		  res.status(err.status || 500);
 		  if (err.status === 401) {
 			  return res.render('welcome');
 		  }
+		  
+		  res.status(err.status || 500);
 		  res.render('error', { pageTitle: `Error ${err.status}`, error: err.message });
 		});
 
