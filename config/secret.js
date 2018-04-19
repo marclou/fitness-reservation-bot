9809e9d94f3ebd4cb542dad7eef2b2dc972b5d72
marclou <marc.louvion@gmail.com>
2018-04-19 15:56:56 +0900

    adding facebook page and app tokens

--- config/secret.js
+++ config/secret.js
@@ -4,4 +4,7 @@ module.exports = {
         'marc.louvion@gmail.com',
         'wpwjh810@gmail.com',
     ],
+    APP_ID: '136609443855499',
+    VERIFY_TOKEN: 'IAmStillReallyHot',
+    PAGE_ACCESS_TOKEN: 'EAAB8Pt1fvIsBAAAKA2nVOKewW9UclQNdlFjANNdZBQfcPMVqZBZCRpIgqKwLjpGRR1kCbnGutuioZBM4ifes6Mx5vRZCJp4BOSb644FqALfZAzYe43k8fVBydeXnMhxAlxbi7NI8kZAAZCMI8N8QXyqJ8A8rZCgAWkZBDD6lZAqCgxXIQZDZD',
 };
