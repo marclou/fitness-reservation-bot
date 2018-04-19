843bf32e5ee72b9c4f46a128d740bfa25ae7dfbe
marclou <marc.louvion@gmail.com>
2018-04-19 17:56:22 +0900

    adding typing on

--- server/controllers/webhookController.js
+++ server/controllers/webhookController.js
@@ -1,9 +1,24 @@
 const request = require('request');
 const config = require('./../../config');
 
+const sendTypeOn = (sender_psid) => {
+    request({
+        'uri': 'https://graph.facebook.com/v2.6/me/messages',
+        'qs': { 'access_token': config.PAGE_ACCESS_TOKEN },
+        'method': 'POST',
+        'json': {
+            'recipient': {
+                'id': sender_psid,
+            },
+            'sender_action': 'typing_on',
+        },
+    });
+};
+
 // Sends response messages via the Send API
 const callSendAPI = (sender_psid, response) => {
     const request_body = {
+        'messaging_type': 'RESPONSE',
         'recipient': {
             'id': sender_psid,
         },
@@ -27,6 +42,8 @@ const callSendAPI = (sender_psid, response) => {
 // Handles messages events
 const handleMessage = (sender_psid, received_message) => {
     let response;
+    
+    sendTypeOn(sender_psid);
     if (received_message.text) {
         response = {
             'text': `You sent the message: "${received_message.text}". Now send me an image!`,
@@ -39,6 +56,7 @@ const handleMessage = (sender_psid, received_message) => {
 const handlePostback = (sender_psid, received_postback) => {
 
 };
+
 
 module.exports = {
     handleMessage,
