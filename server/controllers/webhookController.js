57a7484d15aed57b08dbff8d2cab04f30fcb42c0
marclou <marc.louvion@gmail.com>
2018-04-20 13:00:29 +0900

    adding typing on UI for message processing

--- server/controllers/webhookController.js
+++ server/controllers/webhookController.js
@@ -1,49 +1,49 @@
 const request = require('request');
 const config = require('./../../config');
 
-const sendTypeOn = (sender_psid) => {
-    request({
-        'uri': 'https://graph.facebook.com/v2.6/me/messages',
-        'qs': { 'access_token': config.PAGE_ACCESS_TOKEN },
-        'method': 'POST',
-        'json': {
-            'recipient': {
-                'id': sender_psid,
-            },
-            'sender_action': 'typing_on',
-        },
-    });
+// Call Facebook Send API.
+const callSendAPI = (response) => {
+	request({
+		uri: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {
+			access_token: config.PAGE_ACCESS_TOKEN,
+		},
+		method: 'POST',
+		json: response,
+
+	}, (error, res, body) => {
+		if (!error && response.statusCode === 200) {
+			const recipientId = body.recipient_id;
+			const messageId = body.message_id;
+
+			if (messageId) {
+				console.log('Successfully sent message with id %s to recipient %s', messageId, recipientId);
+			} else {
+				console.log('Successfully called Send API for recipient %s', recipientId);
+			}
+		} else {
+			console.error('Failed calling Send API', response.statusCode, response.statusMessage, body.error);
+		}
+	});
 };
 
-// Sends response messages via the Send API
-const callSendAPI = (sender_psid, response) => {
-    const request_body = {
-        'messaging_type': 'RESPONSE',
-        'recipient': {
-            'id': sender_psid,
-        },
-        'message': response,
-    };
+// Send the '3 dots' to let user knows his message is processing.
+const sendTypingOn = (sender_psid) => {
+	const messageData = {
+		recipient: {
+			id: sender_psid,
+		},
+		sender_action: 'typing_on',
+	};
 
-    request({
-        'uri': 'https://graph.facebook.com/v2.6/me/messages',
-        'qs': { 'access_token': config.PAGE_ACCESS_TOKEN },
-        'method': 'POST',
-        'json': request_body,
-    }, (err, res, body) => {
-        if (!err) {
-            console.log('message sent!');
-        } else {
-            console.error(`Unable to send message:${err}`);
-        }
-    });
+	callSendAPI(messageData);
 };
 
 // Handles messages events
 const handleMessage = (sender_psid, received_message) => {
     let response;
-    
-    sendTypeOn(sender_psid);
+
+    sendTypingOn(sender_psid);
     if (received_message.text) {
         response = {
             'text': `You sent the message: "${received_message.text}". Now send me an image!`,
